15757eea8935fb2d1d16efdf68ba3b98f6068b2a
t375 <56542543+t375@users.noreply.github.com>
2019-10-14 14:01:17 +0200

    Update CaesarCipher.java
    Now the user can decide wether to save the file with the encrypted message or not

--- Java/CaesarCipher.java
+++ Java/CaesarCipher.java
@@ -13,17 +13,21 @@ public class CaesarCipher {
 
 	public static void main(String[] args) {
 		String encryptedText;
+		Boolean save = false;
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter a text to cipher");
 		String text = sc.nextLine();
 		System.out.println("Enter now the key (number) to cipher the message");
 		int key = sc.nextInt();
+		System.out.println("If you want to save the message to a file, type 'y'");
+		save = sc.next().toLowerCase().charAt(0) == 'y' ? true : false;
 		System.out.println("Encrypted message with key " + key + ":");
 		encryptedText = encrypt(text, key, true);
 		System.out.println(encryptedText);
 		System.out.println("The original message was:");
 		System.out.println(encrypt(encryptedText, key, false));
-		saveToFile(encryptedText);
+		if (save)
+			saveToFile(encryptedText);
 	}
 
 	public static String encrypt(String text, int key, boolean encrypting) {
@@ -53,19 +57,19 @@ public class CaesarCipher {
 		}
 		return encrypted;
 	}
-	
+
 	private static void saveToFile(String encryptedText) {
-	 try {
-		Writer writer = new BufferedWriter(new OutputStreamWriter(
-		              new FileOutputStream("encryptedMessage.txt"), "utf-8"));
-		writer.write(encryptedText);
-		writer.close();
-	} catch (UnsupportedEncodingException e) {
-		e.printStackTrace();
-	} catch (FileNotFoundException e) {
-		e.printStackTrace();
-	} catch (IOException e) {
-		e.printStackTrace();
-	}
+		try {
+			Writer writer = new BufferedWriter(
+					new OutputStreamWriter(new FileOutputStream("encryptedMessage.txt"), "utf-8"));
+			writer.write(encryptedText);
+			writer.close();
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
